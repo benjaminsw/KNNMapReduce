c65f220d78cf7aca657d706b876abd976f79defe
benjaminsw <benjaminsw@live.com>
2016-03-07 10:13:08 +0000

    done computing total distance
    finish computeDistance class. Next emit results from mapper.

--- KNNMapReduce.java
+++ KNNMapReduce.java
@@ -1,32 +1,28 @@
 import java.io.BufferedReader;
-
+import java.util.StringTokenizer;
 import javax.xml.soap.Text;
 
 //package KNNMapReduce;
 
 public class KNNMapReduce {
 	
-	//variable test is to store the testing data
-	//http://stackoverflow.com/questions/10416653/best-way-to-store-a-table-of-data
-    private ArrayList<rowData> test = new ArrayList<rowData>();
-    
     //class row to create instance for each row
-    public static class rowData
+    public static class RowData
     {	int age;
     	int income;
     	String marriage;
     	String gender;
     	int children;
     	
-    	public void rowData(){}
-    	public void rowData(String rowInput)
+    	public RowData(){}
+    	public RowData(String rowInput)
     	{
     		String[] features = rowInput.split(" ");
     		this.age = Integer.parseInt(features[0]);
     		this.income = Integer.parseInt(features[1]);
     		this.marriage = features[2];
     		this.gender = features[3];
-    		this.children = Integer.parseInt(features[4]);
+    		this.children = features[4];
     	}
     	public int getAge(){
     		return age;
@@ -40,14 +36,88 @@ public class KNNMapReduce {
     	public String getGender(){
     		return gender;
     	}
-    	public int getChildren(){
+    	public String getChildren(){
     		return children;
     	}
     }
-	
+    
+	//overwrite IntWritable to write distance and label
+    public static class ComputeDistance
+    {
+    	double trainAge, testAge;
+    	double trainIncome, testIncome;
+    	String trainMarriage, testMarrige;
+    	String trainGender, testGender;
+    	double trainChildren, testChildren;
+    	String trainLabel;
+    	double totalDistance;
+    	public ComputeDistance(){}
+    	public ComputeDistance(double testAge, double testIncome, String testMarriage, String testGender, String testChildren,
+    							double trainAge, double trainIncome, String trainMarriage, String trainGender, String trainChildren)
+    	{
+    		this.testAge = testAge;
+    		this.testIncome = testIncome;
+    		this.testMarrige = testMarriage;
+    		this.testGender = testGender;
+    		this.testChildren = testChildren;
+    		this.trainAge = trainAge;
+    		this.trainIncome = trainIncome;
+    		this.trainMarriage = trainMarriage;
+    		this.trainGender = trainGender;
+    		this.trainChildren = trainChildren;
+    	}
+
+    	public double pseudoEuclideanDist(x1,x2)
+    	{
+    		return Math.pow((x1-x2),2);
+    	}
+    	public double hammingDist(String a Stirng b)
+    	{
+    		if(a==b)
+    		{
+    			return 0;
+    		}else
+    		{
+    			return 1;
+    		}
+    	}
+    	public void computeDistance()
+    	{
+    		totalDistance = Math.sqrt(pseudoEuclideanDist(trainAge, testAge)+ pseudoEuclideanDist(trainIncome, testIncome))+
+    						hammingDist(trainMarriage, testMarriage)+hammingDist(trainGender, testGender)+
+    						hammingDist(trainChildren, testChildren);
+    	}
+    	public double getTotalDistance(){
+    		return totalDistance;
+    	}
+    	
+    }
+    
 	//class KNNMapper for processing map step
 	public static class KNNMapper extends Mapper<Object, Text, Text, IntWritable>
-	{	
+	{		
+		//variable test is to store the testing data
+		//http://stackoverflow.com/questions/10416653/best-way-to-store-a-table-of-data
+	    private ArrayList<RowData> test = new ArrayList<RowData>();
+	    double trainAge, testAge;
+	    double trainIncome, testIncome;
+	    String trainMarriage, testMarriage;
+	    String trainGender, testGender;
+	    String trainChildren, testChildren;
+	    String trainLabel, testLabel;
+	    double totalDist;
+	    
+	    //ranges of continuous input data
+	    double minAge = 18;
+	    double maxAge = 77;
+	    double minIncome = 50000;
+	    double maxIncome = 67789;
+	    
+		//normalise continous data
+		public double scaling(double x, double min, double max){
+			return (x-min)/(max-min);
+		}
+	    
 		//the setup function is run once pre-processing data(get test set)
 		public void setup(Context context)throws IOException
 		{	
@@ -55,24 +125,46 @@ public class KNNMapReduce {
 			Configuration conf = context.getConfiguaration();
 			URI[] cacheFiles = context.getCacheFiles();
 			String [] fn = cacheFiles[0].toString().split('#');
+			String str;
 			BufferedReader br = new BufferedRedaer(new FileReader(fn[1]));//localname??
-			//int count = 0//keep records of number of line readin so far
+			str = br.readLine();
 			while(br!=null){
-				test.add(new rowData(br));
 				//add data to data structure
-				//count++;
+				test.add(new RowData(str));
+				str = br.readLine();
 			}
 			br.close();
 		}
+
 		//perform map step
-		public void map(Object key, Text value, Context context)throws IOException, InteruptedException
+		public void mapper(Object key, Text value, Context context)throws IOException, InteruptedException
 		{
-			//implement
+			String rLine = value.toString();
+			StringTokenizer tokens = new StringTokenizer(rLine, ",");
+			trainAge = scaling(Double.parseDouble(tokens.nextToken()),minAge, maxAge);
+			trainIncome = scaling(Double.parseDouble(tokens.nextToken()), minIncome, maxIncome);
+			trainMarriage = tokens.nextToken();
+			trainGender = tokens.nextToken();
+			trainChildren = tokens.nextToken();
+			trainLabel = tokens.nextToken();
+			for(RowData t:test)
+			{	testAge = scaling(t.getAge(),minAge, maxAge);
+				testIncome = scaling(t.getIncome(), minIncome, maxIncome);
+				testMarriage = t.getMarriage();
+				testGender = t.getGender();
+				testChildren = t.getChildren();
+				ComputeDistance dist = new ComputeDistance(testAge, testIncome, testMarriage, testGender, testChildren,
+															trainAge, trainIncome, trainMarriage, trainGender, trainChildren);
+				//totalDist = dist.getTotalDistance
+				//context.write(rLine, )
+				
+			}
+			
 		}
 	}
 	//---------------------END MAP------------------------
 	public static class KNNReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
-		public void reduce(Text key, Iterable<IntWritable> values, Context cotext)throws IOException, InteruptedException
+		public void reducer(Text key, Iterable<IntWritable> values, Context cotext)throws IOException, InteruptedException
 		{
 			//implement
 		}
