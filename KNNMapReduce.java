735a277ae823b43040157a063b8e315462a7d2ae
benjaminsw <benjaminsw@live.com>
2016-03-16 02:43:47 +0000

    sorted hashmap in reducer

--- KNNMapReduce.java
+++ KNNMapReduce.java
@@ -225,9 +225,42 @@ public class KNNMapReduce {
 				String[] keyvalue = values.split(","); 
 				labelDistTuple.put(keyvalue[0], Integer.parseInt(keyvalue[1]));
 			}
+			LinkedHashMap sortedDistLabel = sortHashMapByValuesD(labelDistTuple);
 			
 			
 		}
+		//sort HashMap : http://stackoverflow.com/questions/8119366/sorting-hashmap-by-values
+		public LinkedHashMap sortHashMapByValuesD(HashMap passedMap) {
+			   List mapKeys = new ArrayList(passedMap.keySet());
+			   List mapValues = new ArrayList(passedMap.values());
+			   Collections.sort(mapValues);
+			   Collections.sort(mapKeys);
+
+			   LinkedHashMap sortedMap = new LinkedHashMap();
+
+			   Iterator valueIt = mapValues.iterator();
+			   while (valueIt.hasNext()) {
+			       Object val = valueIt.next();
+			       Iterator keyIt = mapKeys.iterator();
+
+			       while (keyIt.hasNext()) {
+			           Object key = keyIt.next();
+			           String comp1 = passedMap.get(key).toString();
+			           String comp2 = val.toString();
+
+			           if (comp1.equals(comp2)){
+			               passedMap.remove(key);
+			               mapKeys.remove(key);
+			               sortedMap.put((String)key, (Double)val);
+			               break;
+			           }
+
+			       }
+
+			   }
+			   return sortedMap;
+			}
+		
 	}
 	//--------------------END REDUCE----------------------
 	public static void main(String[] args) throws Exception{
